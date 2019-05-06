13ad0be1b272fd01d421d9df3b3ced3fcb02f947
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-05-06 16:32:50 +0300

    Update project

---
+++ js/main.js
@@ -0,0 +1,106 @@
+$('.slider').slick({
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1199,
+      settings: {
+        arrows: false
+      }
+    }, {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+  ]
+});
+$('.review-slider').slick({
+  centerMode: true,
+  centerPadding: '50px',
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '.prev',
+  nextArrow: '.next',
+  responsive: [{
+      breakpoint: 991,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+$('.single-slider').slick({
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 5000
+});
+
+(jQuery);
+(function ($) {
+  $(function () {
+
+    $('select').styler();
+
+  });
+})(jQuery);
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $('#buttonUp').fadeIn().addClass('active');
+  } else {
+    $('#buttonUp').fadeOut().removeClass('active');
+  }
+});
+
+$('#buttonUp').click(function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
